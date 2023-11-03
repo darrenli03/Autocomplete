2bb684ec40b8b7602913d73d74e31675f619d235
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2023-11-03 03:41:52 -0400

    standard implementation of PrefixComparator.compare() as described in readMe

--- src/PrefixComparator.java
+++ src/PrefixComparator.java
@@ -44,48 +44,48 @@ public class PrefixComparator implements Comparator<Term> {
      * @return < 0 if v < w, == 0 if v == w, and > 0 if v > w
      */
     public int compare(Term v, Term w) {
-//        String wordV = v.getWord();
-//        String wordW = w.getWord();
-//
-//        int shorterLength = Math.min(wordV.length(), wordW.length());
-//
-//        if(shorterLength < myPrefixSize){
-//            return wordV.compareTo(wordW);
-//        }
-//
-//        for(int i=0; i<myPrefixSize; i++){
-//
-//            char charV = wordV.charAt(i);
-//            char charW = wordW.charAt(i);
-//
-//            //detects char where prefixes are not equal, determines which word should go first
-//            if(charV!=charW){
-//                return charV - charW;
-//            }
-//
-//        }
-//        //if both prefixes are equal
-//        return 0;
-
-
         String wordV = v.getWord();
-//        System.out.println(wordV);
         String wordW = w.getWord();
-//        System.out.println(wordW + "\n");
 
-        // if either word is shorter than prefix, compare the word lengths
-        if (wordV.length() < myPrefixSize || wordW.length() < myPrefixSize) {
-//            this works because of override in Term class
-//            System.out.println(v.compareTo(w));
-            return v.compareTo(w);
-//            return wordV.compareTo(wordW);
+        int shorterLength = Math.min(wordV.length(), wordW.length());
 
-        } else {//prefix shorter or same size as both words
-            String prefixV = v.getWord().substring(0, myPrefixSize);
-            String prefixW = w.getWord().substring(0, myPrefixSize);
-//            System.out.println("used prefixes: " + prefixV.compareTo(prefixW));
-            return prefixV.compareTo(prefixW);
+        if(shorterLength < myPrefixSize){
+            return wordV.compareTo(wordW);
         }
+
+        for(int i=0; i<myPrefixSize; i++){
+
+            char charV = wordV.charAt(i);
+            char charW = wordW.charAt(i);
+
+            //detects char where prefixes are not equal, determines which word should go first
+            if(charV!=charW){
+                return charV - charW;
+            }
+
+        }
+        //if both prefixes are equal
+        return 0;
+
+
+//        String wordV = v.getWord();
+////        System.out.println(wordV);
+//        String wordW = w.getWord();
+////        System.out.println(wordW + "\n");
+//
+//        // if either word is shorter than prefix, compare the word lengths
+//        if (wordV.length() < myPrefixSize || wordW.length() < myPrefixSize) {
+////            this works because of override in Term class
+////            System.out.println(v.compareTo(w));
+//            return v.compareTo(w);
+////            return wordV.compareTo(wordW);
+//
+//        } else {//prefix shorter or same size as both words
+//            String prefixV = v.getWord().substring(0, myPrefixSize);
+//            String prefixW = w.getWord().substring(0, myPrefixSize);
+////            System.out.println("used prefixes: " + prefixV.compareTo(prefixW));
+//            return prefixV.compareTo(prefixW);
+//        }
 
     }
 }
