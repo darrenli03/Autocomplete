9a018ee007dae0c528046382a10a0558c8e6513b
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2023-11-05 17:04:11 -0500

    implemented initialize and constructor

--- src/HashListAutocomplete.java
+++ src/HashListAutocomplete.java
@@ -1,9 +1,16 @@
-import java.util.List;
+import java.util.*;
 
 public class HashListAutocomplete implements Autocompletor {
+    private static final int MAX_PREFIX = 10;
+    private Map<String, List<Term>> myMap;
+    private int mySize;
 
     public HashListAutocomplete(String[] terms, double[] weights) {
-    
+        if (terms == null || weights == null) {
+            throw new NullPointerException("One or more arguments null");
+        }
+
+        initialize(terms,weights);
     }
 
     @Override
@@ -14,8 +21,16 @@ public class HashListAutocomplete implements Autocompletor {
 
     @Override
     public void initialize(String[] terms, double[] weights) {
-        // TODO Auto-generated method stub
-        
+        myMap = new HashMap<String, List<Term>>();
+        //iterate over all terms (and corresponding weights)
+        for(int i=0; i<terms.length; i++){
+            Term currentTerm = new Term(terms[i], weights[i]);
+            //add term to each possible prefix key in map
+            for(int j=0; j <= Math.min(MAX_PREFIX, currentTerm.getWord().length()); j++){
+                String prefix = (j==0) ? "" : currentTerm.getWord().substring(0, j);
+                myMap.getOrDefault(prefix, new ArrayList<Term>()).add(currentTerm);
+            }
+        }
     }
 
     @Override
