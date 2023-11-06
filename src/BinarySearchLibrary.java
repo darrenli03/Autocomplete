cf7458f62410f1c4efda6cce9c1fb44695b9409d
darrenli03 <71399017+darrenli03@users.noreply.github.com>
2023-11-05 19:55:39 -0500

    edge case for if list is empty in firstIndex and lastIndex

--- src/BinarySearchLibrary.java
+++ src/BinarySearchLibrary.java
@@ -46,7 +46,7 @@ public class BinarySearchLibrary {
 	 */
 	public static <T> int firstIndex(List<T> list,
 			T target, Comparator<T> comp) {
-
+		if(list.size() == 0) return -1;
 		int low = -1;
 		int high = list.size()-1;
 		while (low+1 != high) {
@@ -85,6 +85,7 @@ public class BinarySearchLibrary {
 	 */
 	public static <T> int lastIndex(List<T> list,
 			T target, Comparator<T> comp) {
+		if(list.size() == 0) return -1;
 
 		int low = 0;
 		int high = list.size();
